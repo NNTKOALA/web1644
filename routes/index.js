80cc8258dcaaa39c56985620fda6f56295ddc6f3
NNTKOALA <trungnngch200639@fpt.edu.vn>
2022-10-22 13:47:36 +0700

    asm

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,17 @@ const express = require('express')
 const ToyShopModel = require('../models/ToyShopModel')
 const router = express.Router()
 
-// //set trang chủ (homepage)
-// router.get('/', (req, res) => {
-//   //render ra trang index ở trong thư mục views
-//   res.render('index')
-// })
+//set trang chủ (homepage)
+router.get('/', (req, res) => {
+  //render ra trang index ở trong thư mục views
+  res.render('index')
+})
 
 //URL: localhost:3000/ToyShop
-router.get('/', (req, res) => {
+router.get('/homepage', (req, res) => {
   ToyShopModel.find((err, data) => {
     if (!err) {
-      res.render('index', { ToyShop: data })
+      res.render('homepage', { ToyShop: data })
     }
   })
 })
