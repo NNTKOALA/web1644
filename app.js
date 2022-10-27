35046542b9a0a17d3573c916552047fd5537874e
NNTKOALA <trungnngch200639@fpt.edu.vn>
2022-10-27 14:34:46 +0700

    asm

--- app.js
+++ app.js
@@ -5,45 +5,30 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var ToyShopRouter = require('./routes/ToyShop');
-
-var mongoose = require('mongoose');
-var url = "mongodb+srv://nguyentrung:nntrung382k2@cluster.8b5c38m.mongodb.net/ATN"
-//var url = "mongodb://localhost:27017/ATN"
-
-mongoose.connect(url, { useNewUrlParser: true }, err => {
-  if (!err) {
-    console.log("DB connect success")
-  } else {
-    console.error(err)
-  }
-})
+var usersRouter = require('./routes/users');
 
 var app = express();
-
-var bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: false }))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/ToyShop', ToyShopRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -52,10 +37,5 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-  console.log("http://localhost:3000")
-})
 
 module.exports = app;
